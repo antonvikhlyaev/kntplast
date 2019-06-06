05b497d591a52975fb1b3f5d8ba8ac06eba1621b
Anton Vikhlyaev <antonvikhlyaev@gmail.com>
2019-06-07 00:18:22 +0500

    Добавляет полную стилизацию всех страниц

--- source/js/main.js
+++ source/js/main.js
@@ -11,3 +11,33 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 })
+
+
+// Всплывающее меню
+var ceilingButton = document.querySelector('.sorting__button--ceiling');
+var ceilingDropdown = document.querySelector('.sorting__dropdown--ceiling');
+
+var furnitureButton = document.querySelector('.sorting__button--furniture');
+var furnitureDropdown = document.querySelector('.sorting__dropdown--furniture');
+
+ceilingButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (ceilingDropdown.classList.contains('dropdown--closed')) {
+    ceilingDropdown.classList.remove('dropdown--closed');
+    ceilingDropdown.classList.add('dropdown--opened');
+  } else {
+    ceilingDropdown.classList.add('dropdown--closed');
+    ceilingDropdown.classList.remove('dropdown--opened');
+  }
+})
+
+furnitureButton.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (furnitureDropdown.classList.contains('dropdown--closed')) {
+    furnitureDropdown.classList.remove('dropdown--closed');
+    furnitureDropdown.classList.add('dropdown--opened');
+  } else {
+    furnitureDropdown.classList.add('dropdown--closed');
+    furnitureDropdown.classList.remove('dropdown--opened');
+  }
+})
