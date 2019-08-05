6c3ec388f68ac0f5731c4dd13a1e5b54c8eaef4b
Anton Vikhlyaev <antonvikhlyaev@gmail.com>
2019-08-05 18:57:55 +0500

    Обновляет сборку

---
+++ build/js/open-menu.js
@@ -0,0 +1,13 @@
+// Открытие меню
+var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.main-nav');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+})
